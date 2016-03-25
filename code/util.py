e55c6e1a0fc5528ba8046ba105dd1412733267aa
Guido Sanchez <sanchez.guido@gmail.com>
2016-03-25 12:33:24 -0300

    Add mtimes function from Rawlings' mpc-toolbox-casadi.

--- code/util.py
+++ code/util.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.linalg
+import casadi.tools as ctools
 
 
 def c2d(A, B, Delta, Bp=None, f=None, asdict=False):
@@ -30,3 +31,41 @@ def c2d(A, B, Delta, Bp=None, f=None, asdict=False):
     else:
         retval = [Ad, Bd, Bpd, fd]
     return retval
+
+
+def mtimes(*args, **kwargs):
+    """
+    Smarter version casadi.tools.mtimes.
+
+    Matrix multiplies all of the given arguments and returns the result. If any
+    inputs are Casadi's SX or MX data types, uses Casadi's mtimes. Otherwise,
+    uses a sequence of np.dot operations.
+
+    Keyword arguments forcedot or forcemtimes can be set to True to pick one
+    behavior or another.
+    """
+    # Get keyword arguments.
+    forcemtimes = kwargs.pop("forcemtimes", None)
+    forcedot = kwargs.pop("forcedot", False)
+    if len(kwargs) > 0:
+        raise TypeError("Invalid keywords: %s" % kwargs.keys())
+
+    # Pick whether to use mul or dot.
+    if forcemtimes:
+        if forcedot:
+            raise ValueError("forcemtimes and forcedot can't both be True!")
+        useMul = True
+    elif forcedot:
+        useMul = False
+    else:
+        useMul = False
+        symtypes = set(["SX", "MX"])
+        for a in args:
+            atype = getattr(a, "type_name", lambda : None)()
+            if atype in symtypes:
+                useMul = True
+                break
+
+    # Now actually do multiplication.
+    ans = ctools.mtimes(args) if useMul else reduce(np.dot, args)
+    return ans

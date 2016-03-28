63e956a52153bf4193602bb1444e86f4b3cc7e22
Guido Sanchez <sanchez.guido@gmail.com>
2016-03-28 15:33:46 -0300

    Working example with the differential drive robot model using nmhe_ltv.

--- code/nmhe_ltv_test.py
+++ code/nmhe_ltv_test.py
@@ -2,7 +2,8 @@ import casadi
 import casadi.tools as ctools
 import numpy as np
 import scipy.linalg
-import model_dcmotor as model
+# import model_dcmotor as model
+import model_lego as model
 import tools
 import util
 import matplotlib.pyplot as plt
@@ -26,8 +27,8 @@ Delta = 0.1
 Nt = 10         # Horizon size
 Nsim = 80
 
-sigma_w = 0.05   # Standard deviation for the process noise
-sigma_v = 0.25  # Standard deviation of the measurements
+sigma_w = 0.0001   # Standard deviation for the process noise
+sigma_v = np.deg2rad(0.5)  # Standard deviation of the measurements
 sigma_p = 0.5    # Standard deviation for prior
 
 f_casadi = tools.getCasadiFunc(model.F, [Nx, Nu, Nw], ["x", "u", "w"], "f", rk4=False)
@@ -46,10 +47,13 @@ def _calc_lin_disc_wrapper_for_mp_map(item):
 
 x0 = np.zeros((Nx,))
 
-wsim = sigma_w*np.fabs(np.random.randn(Nsim, Nw))
+wsim = 0.0*sigma_w*np.fabs(np.random.randn(Nsim, Nw))
 vsim = 0.0*sigma_v*(np.random.rand(Nsim, Nv)-0.5)
 
-usim = 20.0*np.ones((Nsim, Nu))
+usim = [40.0, 40.0]*np.ones((Nsim, Nu))
+usim[0:25,0] = np.linspace(0,40,25)
+usim[0:25,1] = np.linspace(0,40,25)
+# usim[0:,0] =
 ysim = np.zeros((Nsim, Ny))
 
 # xsim_rk4 = np.zeros((Nsim+1, Nx))
@@ -61,7 +65,8 @@ xsim_ltv[0,:] = x0
 
 for t in range(Nsim):
     # Measure.
-    ysim[t] = np.around(model.H(xsim_ltv[t,:]) + vsim[t,:], decimals=0)
+    # ysim[t] = model.H(xsim_ltv[t,:]) + vsim[t,:]
+    ysim[t] = np.deg2rad( np.around( np.rad2deg( model.H(xsim_ltv[t,:]) + vsim[t,:]), decimals=0))
     # Simulate with CasADi integrator.
     # xsim_int[t+1,:] = f_casadi_int(xsim_int[t,:], usim[t,:], wsim[t,:]).full().ravel() #F_integrator(x0=xsim_int[t,:],p=np.hstack((usim[t,:],wsim[t,:])))['xf'].full().ravel()
     # xsim_rk4[t+1,:] = f_casadi_rk4(xsim_rk4[t,:], usim[t,:], wsim[t,:]).full().ravel()
@@ -110,10 +115,11 @@ if doSimPlots:
 
 Q = np.diag((sigma_w*np.ones((Nw,)))**2)
 R = np.diag((sigma_v*np.ones((Nv,)))**2)
+# Q = np.diag([0.001, 0.001, np.deg2rad(0.5), 1.0, 0.01, 0.01, 1.0, 0.01, 0.01])
 Qinv = scipy.linalg.inv(Q)
 Rinv = scipy.linalg.inv(R)
 P = np.diag((sigma_p*np.ones((Nx,)))**2)
-x_0 = x0 + 3.0*sigma_p*np.random.randn(Nx)
+x_0 = x0 + 0.0*sigma_p*np.random.randn(Nx)
 
 def lfunc(w, v):
     return util.mtimes(w.T, Qinv, w) + util.mtimes(v.T, Rinv, v)
@@ -139,8 +145,8 @@ ltvStruct = ctools.struct_symSX([  # ctools.entry("y", repeat=N["t"] + 1, shape=
 ltv_guess = ltvStruct(0)
 curr_sol = varStruct(0)
 curr_sol["x",0] = x_0
-xhat_ltv = []
-xhat_ltv.append(x_0)
+xhat_ltv = np.zeros((Nsim,Nx))
+xhat_ltv[0,:] = x_0
 
 for t in range(Nsim):
     # Define sizes of everything.
@@ -156,26 +162,32 @@ for t in range(Nsim):
     print (tmin, tmax)
     starttime = time.clock()
     # print "ltv_len: ",len(range(N["t"]))
+    ltv_guess = ltvStruct(0)
+
     for k in range(N["t"]):
         ltv_guess["Ad",k], ltv_guess["Bd",k], ltv_guess["Gd",k], ltv_guess["fd",k] =\
-            _calc_lin_disc_wrapper_for_mp_map((curr_sol["x",k], usim[k+tmin, :], curr_sol["w",k], Delta))
+        _calc_lin_disc_wrapper_for_mp_map((curr_sol["x",k], usim[k+tmin, :], curr_sol["w",k], Delta))
+            #_calc_lin_disc_wrapper_for_mp_map((xsim_ltv[k+tmin,:], usim[k+tmin, :], wsim[k+tmin,:], Delta))
 
     # for k in ltv_guess.keys():
     #     print k, ltv_guess[k]
 
     sol = tools.nmhe_ltv(f_casadi, h_casadi, usim[tmin:tmax-1,:], ysim[tmin:tmax,:], l, N,
-                   lx=lx, x0bar=x_0, P0=linalg.inv(P), Delta=Delta, ltv_guess=ltv_guess, returnSolver=False)
+                   lx=lx, x0bar=x_0, P0=linalg.inv(P), Delta=Delta, ltv_guess=ltv_guess, guess=None, returnSolver=False)
 
+    xhat_ltv[t,:] = sol["x"][-1].full().ravel()
+
+    curr_sol = varStruct(0)
     for k in sol.keys():
         for i in range(len(sol[k])):
             curr_sol[k,i] = sol[k,i]
 
     if not fullInformation and t + 1 > Nt:
-        for k in sol.keys():
-            curr_sol[k,0:-1] = sol[k,1:]
-            curr_sol[k,-1] = sol[k,-1]
+        # for k in sol.keys():
+            # curr_sol[k,0:-1] = sol[k,1:]
+            # curr_sol[k,-1] = sol[k,-1]
         x_0 = curr_sol["x",1]
-        xhat_ltv.append(np.array(curr_sol["x", 0]).flatten())
+        # xhat_ltv.append(np.array(curr_sol["x", 0]).flatten())
     # else:
     #     curr_sol['x',0:tmax] = sol['x',0:tmax]
 
@@ -183,8 +195,8 @@ for t in range(Nsim):
     print "%3d (%10.5g s)" % (t, time.clock() - starttime)
     # print "%3d (%10.5g s): %s" % (t, time.clock() - starttime, sol["status"])
 
-for i in xrange(1,len(curr_sol["x"])):
-    xhat_ltv.append(np.array(curr_sol["x",i]).flatten())
+# for i in xrange(1,len(curr_sol["x"])):
+#    xhat_ltv.append(np.array(curr_sol["x",i]).flatten())
 
 xhat_ltv = np.array(xhat_ltv)
 
@@ -194,7 +206,7 @@ if doMHEPlots:
 
     for i in range(Nx):
         thisPos = np.unravel_index(i, pltDim, order='F')
-        axarr[thisPos].plot(xsim_ltv[:,i], 'k--', label='ltv')
+        axarr[thisPos].plot(xsim_ltv[:-1,i], 'k--', label='ltv')
         axarr[thisPos].plot(xhat_ltv[:,i], 'k:.', label='mhe')
         # axarr[thisPos].plot(xsim_rk4[:,i], 'k:o', label='rk4')
         axarr[thisPos].set_ylabel('$x[%d]$' % i, fontsize=fontsize)
@@ -203,8 +215,10 @@ if doMHEPlots:
 
     for i in range(Nx):
             thisPos = np.unravel_index(Nx+i, pltDim, order='F')
-            axarr[thisPos].plot((xhat_ltv[:,i]-xsim_ltv[:,i]), 'k--', label='err mhe')
+            axarr[thisPos].plot((xhat_ltv[:,i]-xsim_ltv[:-1,i]), 'k--', label='err mhe')
             # axarr[thisPos].plot((xsim_int[:,i]-xsim_rk4[:,i]), 'k:o', label='err rk4')
             axarr[thisPos].set_ylabel('$x[%d]$' % i, fontsize=fontsize)
             axarr[thisPos].legend(loc="lower right", prop={'size': 8})
             axarr[thisPos].grid()
+
+    plt.suptitle("NMHE LTV")
